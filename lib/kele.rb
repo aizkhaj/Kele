15c8128d88c177bb8399e3ed5318e744b1c5aa73
Aizaz Khaja <aizazkj@gmail.com>
2017-06-22 13:59:46 -0400

    added method for mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,13 +11,20 @@ class Kele
     @password = password
 
     response = self.class.post('/sessions', body: {email: @username, password: @password})
-    # puts response
 
     @auth_token = response['auth_token']
   end
 
   def get_me
     response = self.class.get('/users/me', headers: { authorization: @auth_token })
+    # @mentor_id = response.body['current_enrollment']['mentor_id']
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability
+    mentor_id = get_me['current_enrollment']['mentor_id']
+    raise 'Mentor ID is nonexistent' if mentor_id.nil?
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {authorization: @auth_token})
     JSON.parse(response.body)
   end
 end
